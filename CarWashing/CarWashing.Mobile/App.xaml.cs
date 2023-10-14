ee647d3c5eb6da972ae16e3d4199e83a76ada2c0
luisflorez84571 <luisflorez84571@correo.itm.edu.co>
2023-10-14 09:02:13 -0500

    Primer Commit

---
+++ CarWashing/CarWashing.Mobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CarWashing.Mobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
