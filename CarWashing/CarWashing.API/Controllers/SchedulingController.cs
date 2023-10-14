b2f081759df2066921c5c75deaa0a6fa23904ab9
luisflorez84571 <luisflorez84571@correo.itm.edu.co>
2023-10-14 10:46:11 -0500

    Commit de Controller

---
+++ CarWashing/CarWashing.API/Controllers/SchedulingController.cs
@@ -0,0 +1,94 @@
+﻿using CarWashing.API.Data;
+using CarWashing.Shared.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+[Route("api/[controller]")]
+[ApiController]
+public class SchedulingController : ControllerBase
+{
+    private readonly DataContext _context;
+
+    public SchedulingController(DataContext context)
+    {
+        _context = context;
+    }
+
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Scheduling>>> GetSchedulings()
+    {
+        return await _context.Schedulings.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Scheduling>> GetScheduling(int id)
+    {
+        var scheduling = await _context.Schedulings.FindAsync(id);
+
+        if (scheduling == null)
+        {
+            return NotFound();
+        }
+
+        return scheduling;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Scheduling>> PostScheduling(Scheduling scheduling)
+    {
+        _context.Schedulings.Add(scheduling);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction("GetScheduling", new { id = scheduling.SchedulingId }, scheduling);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutScheduling(int id, Scheduling scheduling)
+    {
+        if (id != scheduling.SchedulingId)
+        {
+            return BadRequest();
+        }
+
+        _context.Entry(scheduling).State = EntityState.Modified;
+
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!SchedulingExists(id))
+            {
+                return NotFound();
+            }
+            else
+            {
+                throw;
+            }
+        }
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteScheduling(int id)
+    {
+        var scheduling = await _context.Schedulings.FindAsync(id);
+        if (scheduling == null)
+        {
+            return NotFound();
+        }
+
+        _context.Schedulings.Remove(scheduling);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    private bool SchedulingExists(int id)
+    {
+        return _context.Schedulings.Any(e => e.SchedulingId == id);
+    }
+}
---
+++ CarWashing/CarWashing.API/Controllers/VehicleController.cs
@@ -0,0 +1,94 @@
+﻿using CarWashing.API.Data;
+using CarWashing.Shared.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+[Route("api/[controller]")]
+[ApiController]
+public class VehicleController : ControllerBase
+{
+    private readonly DataContext _context;
+
+    public VehicleController(DataContext context)
+    {
+        _context = context;
+    }
+
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Vehicle>>> GetVehicles()
+    {
+        return await _context.Vehicles.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Vehicle>> GetVehicle(int id)
+    {
+        var vehicle = await _context.Vehicles.FindAsync(id);
+
+        if (vehicle == null)
+        {
+            return NotFound();
+        }
+
+        return vehicle;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Vehicle>> PostVehicle(Vehicle vehicle)
+    {
+        _context.Vehicles.Add(vehicle);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction("GetVehicle", new { id = vehicle.VehicleId }, vehicle);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutVehicle(int id, Vehicle vehicle)
+    {
+        if (id != vehicle.VehicleId)
+        {
+            return BadRequest();
+        }
+
+        _context.Entry(vehicle).State = EntityState.Modified;
+
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!VehicleExists(id))
+            {
+                return NotFound();
+            }
+            else
+            {
+                throw;
+            }
+        }
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteVehicle(int id)
+    {
+        var vehicle = await _context.Vehicles.FindAsync(id);
+        if (vehicle == null)
+        {
+            return NotFound();
+        }
+
+        _context.Vehicles.Remove(vehicle);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    private bool VehicleExists(int id)
+    {
+        return _context.Vehicles.Any(e => e.VehicleId == id);
+    }
+}
