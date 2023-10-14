68ac0eac91d3d97b4a65679e4649c16de3e22c9a
luisflorez84571 <luisflorez84571@correo.itm.edu.co>
2023-10-14 12:36:45 -0500

    Commit de RazorComponen

--- CarWashing/CarWashing.WEB/Program.cs
+++ CarWashing/CarWashing.WEB/Program.cs
@@ -1,5 +1,6 @@
 using CarWashing.WEB;
 using CarWashing.WEB.Repositories;
+using CurrieTechnologies.Razor.SweetAlert2;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -10,5 +11,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7000/") });
 
 builder.Services.AddScoped<IRepository, Repository>();
+builder.Services.AddSweetAlert2();
 
 await builder.Build().RunAsync();
