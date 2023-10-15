3798494cfd917ce9bbcec0f7cd8a7bb1130b06c2
luisflorez84571 <luisflorez84571@correo.itm.edu.co>
2023-10-15 15:30:30 -0500

    Commit NewMigration

---
+++ CarWashing/CarWashing.API/Data/SeedDb.cs
@@ -0,0 +1,32 @@
+﻿using CarWashing.Shared.Entities;
+
+namespace CarWashing.API.Data
+{
+    public class SeedDb
+    {
+        private readonly DataContext _context;
+
+        public SeedDb(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task SeedAsync()
+        {
+            await _context.Database.EnsureCreatedAsync();
+            await CheckCountriesAsync();
+        }
+
+        private async Task CheckCountriesAsync()
+        {
+            if (!_context.Clients.Any())
+            {
+                _context.Clients.Add(new Client { FirstName = "Fernando" });
+                _context.Clients.Add(new Client { FirstName = "Luis" });
+            }
+
+            await _context.SaveChangesAsync();
+        }
+    }
+}
+
--- CarWashing/CarWashing.API/Program.cs
+++ CarWashing/CarWashing.API/Program.cs
@@ -6,13 +6,25 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=DefaultConnection"));
-
+builder.Services.AddTransient<SeedDb>();
 var app = builder.Build();
+SeedData(app);
+void SeedData(WebApplication app)
+{
+    IServiceScopeFactory scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+
+    using (IServiceScope scope = scopedFactory!.CreateScope())
+    {
+        SeedDb service = scope.ServiceProvider.GetService<SeedDb>();
+        service!.SeedAsync().Wait();
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
