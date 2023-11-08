b07788be248b1de0ad29f25be1ec66751086dd31
luisflorez84571 <luisflorez84571@correo.itm.edu.co>
2023-11-08 10:02:00 -0500

    ActualizacionWEB (08-Nov)

---
+++ CarWashing/CarWashing.Shared/Enums/OrderStatus.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace CarWashing.Shared.Enums
+{
+    public enum OrderStatus
+    {
+        [Description("Nuevo")]
+        New,
+
+        [Description("Despachado")]
+        Dispatched,
+
+        [Description("Enviado")]
+        Sent,
+
+        [Description("Confirmado")]
+        Confirmed,
+
+        [Description("Confirmed")]
+        Cancelled
+    }
+}
